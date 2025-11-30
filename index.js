4f5ca406eed83f7ae7469d584bd2053f496b5bbb
unknown <thachanathbu@gmail.com>
2025-11-30 14:19:37 +0700

    calculaate API

--- index.js
+++ index.js
@@ -6,49 +6,67 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get("/op", (req, res) => {
+app.get("/plus",(req,res)=>{
     const a = Number(req.query.a)
     const b = Number(req.query.b)
-    const op = req.query.op
-    if (op == "+") {
-        result = (a + b)
-    }
-    else if (op == "-") {
-        result = (a - b)
-    }
-    else if (op == "x") {
-        result = (a * b)
-    }
-    else if (op == "/") {
-        result = (a / b)
-    }
-    else if (op == "%") {
-        result = (a % b)
-    }
-    else if (op == "**") {
-        result = (a ** b)
-    }
-    else if (op == "/" && b == 0) {
-        result = ("cannot divide by zero")
+    if (!isNaN(a) && !isNaN(b)){
+        result = a + b
     }
     else {
-        result = ("dont know")
+        result = "Invalid"
     }
     res.json({
         result: result
     })
 })
-app.get("/user", (req, res) => {
-    const name = req.body.name
+
+app.get("/minus",(req,res)=>{
+    const a = Number(req.query.a)
+    const b = Number(req.query.b)
+    if (!isNaN(a) && !isNaN(b)){
+        result = a - b
+    }
+    else {
+        result = "Invalid"
+    }
     res.json({
-        message: 'Hello' + name
+        result: result
     })
 })
-app.get("/",(req,res)=>{
+
+app.get("/X",(req,res)=>{
+    const a = Number(req.query.a)
+    const b = Number(req.query.b)
+    if (!isNaN(a) && !isNaN(b)){
+        result = a * b
+    }
+    else {
+        result = "Invalid"
+    }
     res.json({
-        message: "Hello World"
+        result: result
     })
 })
+
+app.get("/divide",(req,res)=>{
+    const a = Number(req.query.a)
+    const b = Number(req.query.b)
+    if (!isNaN(a) && !isNaN(b)){
+        if ( b != 0){
+            result = a / b
+        }
+        else {
+            result = "Cannot divide by zero"
+        }
+    }
+    else {
+        result = "Invalid"
+    }
+    res.json({
+        result: result
+    })
+})
+
 app.listen(3000, () => {
     console.log('Sever start suceessfully')
 })
